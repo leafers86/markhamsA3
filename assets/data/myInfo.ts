aaafef815b96585af2b917d2fcacd52b24aad869
Steve Markham <markhams@sheridancollege.ca>
2019-07-26 13:09:35 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,8 @@
+import { Markhams } from '../../app/markhams';
+
+export const MYINFO: Markhams =
+  {
+    sid: 991482983, sname: "Steven Markham",
+    slogin: "markhams", scampus: "Trafalgar",
+    atitle: "Assignment 3"
+  }
---
+++ assets/data/mySongs.ts
@@ -0,0 +1,24 @@
+import { SongInfo } from '../../app/songInfo';
+
+export const MYSONGS: SongInfo[] = [
+  {
+    artist: "Ed Sheeran", name: "Beautiful People",
+    genre: "Pop", releaseYear: "2019",
+    picture: "./assets/images/edSheeranBeautifulPeople.png"
+  },
+  {
+    artist: "Ellie Goulding", name: "Close to Me",
+    genre: "Pop", releaseYear: "2018",
+    picture: "./assets/images/CloseToMe.jpg"
+  },
+  {
+    artist: "Eminem", name: "Venom",
+    genre: "Rap", releaseYear: "2019",
+    picture: "./assets/images/eminemvenom.jpeg"
+  },
+  {
+    artist: "21 Pilots", name: "Stressed Out",
+    genre: "Rock, Hip-Hop/Rap", releaseYear: "2015",
+    picture: "./assets/images/stressedout.jpg"
+  }
+]
